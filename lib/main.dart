63d20b3880e1b63808dcdd47b5f50c7f16529822
arunpraseeth <arunpraseeth97@gmail.com>
2022-12-27 02:36:52 -0500

    about to integrate firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,19 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:invoice_web/auth/login.dart';
 import 'package:invoice_web/utils/constants.dart';
 import 'package:invoice_web/home_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: const FirebaseOptions(
+      apiKey: "AIzaSyAMmlPaoEAEGGsWxudIZwYHqxqzmW0PS28",
+      projectId: "invoice-f3d65",
+      messagingSenderId: "570465555594",
+      appId: "1:570465555594:web:287ec2acc4a2577ed80973",
+    ),
+  );
   runApp(const MyApp());
 }
 
